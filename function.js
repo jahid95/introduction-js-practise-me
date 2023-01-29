f361a5e6ed8e69e25ffa83d5e7da8966b513127f
Anwar <anwarjahid449@gmail.com>
2023-01-29 21:25:10 +0600

    js function

---
+++ function.js
@@ -0,0 +1,8 @@
+function bringSingara(money){
+    console.log('singara niye aii !!!');
+    console.log(money);
+}
+
+bringSingara(100);
+
+bringSingara(200+100);
---
+++ substring-includes.js
@@ -0,0 +1,4 @@
+var title ='my name is anwar jahid';
+var substringTitle = title.substring(0,4);
+var isIncludes =title.includes('jahid');
+console.log(isIncludes);
