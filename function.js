0e2509dd68381997310758ff9cb933928a6002e8
Anwar <anwarjahid449@gmail.com>
2023-01-29 21:31:57 +0600

    return function

--- function.js
+++ function.js
@@ -1,8 +1,18 @@
+// function bringSingara(money){
+//     console.log('singara niye aii !!!');
+//     console.log(money);
+// }
+
+// bringSingara(100);
+
+// bringSingara(200+100);
+
 function bringSingara(money){
-    console.log('singara niye aii !!!');
-    console.log(money);
+    var singraPrice = 10;
+    var quantity = money / singraPrice;
+    return quantity;
 }
 
-bringSingara(100);
-
-bringSingara(200+100);
+var myTaka = 4020;
+var singaras =bringSingara(myTaka);
+console.log('singaras:', singaras);
