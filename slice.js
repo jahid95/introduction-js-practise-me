f6dcc6a90442b700432bcc6b4d064e184a0937d9
Anwar <anwarjahid449@gmail.com>
2023-01-29 07:34:43 +0600

    array slice and splice

---
+++ slice.js
@@ -0,0 +1,6 @@
+var numbers = [1,2,3,4,5,6,7,8,9,10];
+var sliceNumbers = numbers.slice(3,6);
+var spliceNumber = numbers.splice(7,2);
+console.log(sliceNumbers);
+console.log(spliceNumber);
+console.log(numbers);
